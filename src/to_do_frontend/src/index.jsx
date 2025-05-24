2a03b7ca3ce1855d2412a6ed49e07aa485983a0d
OSIELJ <osieljuniormartins@gmail.com>
2025-05-24 04:04:17 +0000

    adiciona autenticação

--- src/to_do_frontend/src/index.jsx
+++ src/to_do_frontend/src/index.jsx
@@ -1,25 +1,45 @@
-import { Link } from 'react-router-dom';
+import { AuthClient } from "@dfinity/auth-client";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
-function index() {
-  
-  return (           
+function Index() {
+  const navigate = useNavigate();
 
-    <section class="bg-white dark:bg-gray-900">
-        <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
-            <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">TO-DO</h1>
-            <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">Controle suas tarefas 100% onchain na ICP!</p>
-            <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
-                <Link to="/tarefas/" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
-                    ENTRAR
-                    <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                    </svg>
-                </Link>
-            </div>
+  async function login() {
+    const authClient = await AuthClient.create();
+
+    await authClient.login({
+      identityProvider: "https://identity.ic0.app/#authorize",
+      onSuccess: async () => {
+        // Sucesso na autenticação, redireciona para tarefas
+        navigate("/tarefas/");
+      },
+      windowOpenerFeatures: `
+        left=${window.screen.width / 2 - 525 / 2},
+        top=${window.screen.height / 2 - 705 / 2},
+        toolbar=0,location=0,menubar=0,width=525,height=705
+      `,
+    });
+  }
+
+  return (
+    <section className="bg-white dark:bg-gray-900">
+      <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
+        <h1 className="mb-4 text-4xl font-extrabold">TO-DO</h1>
+        <p className="mb-8 text-lg font-normal text-gray-500">
+          Controle suas tarefas 100% onchain na ICP!
+        </p>
+        <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center">
+          <button
+            onClick={login}
+            className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-white bg-blue-700 hover:bg-blue-800 rounded-lg"
+          >
+            ENTRAR
+          </button>
         </div>
+      </div>
     </section>
-
   );
 }
 
-export default index;
+export default Index;
